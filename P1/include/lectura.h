762e574b93c6f9fe240129945240e7e5c2f0e9d5
Pablo Cordero <pablocorderoromero@gmail.com>
2020-03-04 17:15:58 +0100

    P1 - Makefile hecho

---
+++ P1/include/lectura.h
@@ -0,0 +1,8 @@
+#ifndef LECTURA_H_INCLUDED
+#define LECTURA_H_INCLUDED
+
+#include <iostream>
+
+
+
+#endif
---
+++ P1/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "lectura.h"
+
+using namespace std;
+
+int main(){
+    
+    
+
+}
